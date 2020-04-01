d8b4188ed216fb5ddb6fbf6d8ba2d54f58268500
Anagheem Azzam <anagheemazzam@gmail.com>
2020-04-02 00:38:24 +0300

    test4

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -47,6 +47,7 @@ class _NewTransactionState extends State<NewTransaction> {
       child: Card(
         elevation: 5,
         child: Container(
+          
           padding: EdgeInsets.only(
             top: 10,
             right: 10,
